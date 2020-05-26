960c956f1527c1aa4625bfb0b165d7f006afa9e7
Luz <luz_0614@ifalo.com.tw>
2020-05-26 16:58:49 +0800

    初版

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,28 @@
 <template>
   <div id="app">
-    
+    <nav-bar/>
+    <Home/>
   </div>
 </template>
 
 <script>
-
+import Home from "./components/content/Home"
+import NavBar from './components/common/NavBar'
 
 export default {
   name: 'App',
   components: {
-    
+    Home,
+    NavBar
   }
 }
 </script>
 
 <style>
-
+  body{    
+    margin:0px;
+    padding:0px;
+    background:#fff url( './assets/home/shizu.jpg' ) center center fixed no-repeat;    
+    background-size: 100%, 100%;
+  }
 </style>
---
+++ src/components/common/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div  class="nav-bar">
+      <div class="title">
+        <h2>喜日</h2>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+}
+</script>
+
+<style>
+  .nav-bar {
+    display: flex;
+    height: 60px;
+    background-color: #555555;
+  }
+  
+  .title {
+    font-size:13px;
+    color:#D68B00;
+  }
+
+</style>
